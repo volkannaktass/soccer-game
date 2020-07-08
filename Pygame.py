f0f9a419841ccf9b590d59c86a956f8e29495a35
Volkan Aktas <volkanaktas98@gmail.com>
2020-07-08 14:34:46 +0300

    made scoreboard

--- Pygame.py
+++ Pygame.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+from pygame.locals import *
 
 pygame.init()
 
@@ -9,6 +11,8 @@ green = (0,100,0)
 
 gameDisplay = pygame.display.set_mode((800,600))
 gameDisplay.fill(green)
+myfont = pygame.font.SysFont("monospace",20)
+score = 0
 
 pygame.draw.rect(gameDisplay, black, (30,250,10,125))  #left, top, width, height
 pygame.draw.circle(gameDisplay, white, (400,300), 8) #(left, top), radius
@@ -21,4 +25,14 @@ while True:
             pygame.quit()
             quit()
 
+
+
+    cx = (gameDisplay.get_width() / 2)-120
+    #cy = gameDisplay.get_height() / 2
+    scoretext = myfont.render("Team A: {0} - {0} Team B".format(score), 1, black)
+    #lengthScore = len(scoretext) / 2
+    gameDisplay.blit(scoretext,(cx,0))
+
+    
     pygame.display.update()
+    
