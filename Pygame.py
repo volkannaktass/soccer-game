18f613d8089163b18519bec9082c4e9102d5ef04
volkannaktass <volkanaktas98@gmail.com>
2020-07-08 22:33:40 +0300

    fixed Pygame

--- Pygame.py
+++ Pygame.py
@@ -2,34 +2,20 @@ import pygame
 import sys
 from pygame.locals import *
 
-WIDTH = 800
-HEIGHT = 600
+pygame.init()
+pygame.display.set_caption("PONG")
+gameDisplay  = pygame.display.set_mode((800,600))
 
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 green = (0,100,0)
 
-x1 = 40
 y1 = 250
-x2 = 750
 y2 = 250
-rect_width = 10
-rect_height = 125
-x_circle = 400
-y_circle = 300
-radius = 8
 
-#scoreLeft = 0
-#scoreRight = 0
-
-
-
-pygame.init()
-pygame.display.set_caption("PONG")
-gameDisplay  = pygame.display.set_mode((WIDTH,HEIGHT))
-
-
+scoreLeft = 0
+scoreRight = 0
 myfont = pygame.font.SysFont("monospace",40,bold=True)
 
 run = True
@@ -54,9 +40,9 @@ while run:
 
     gameDisplay.fill(green)
 
-    pygame.draw.rect(gameDisplay, black, (x1, y1, rect_width, rect_height))  # x, y, width, height
-    pygame.draw.circle(gameDisplay, white, (x_circle, y_circle), radius)  # (x, y), radius
-    pygame.draw.rect(gameDisplay, black, (x1, y2, rect_width, rect_height))
+    pygame.draw.rect(gameDisplay, black, (40, y1, 10, 125))  # x, y, width, height
+    pygame.draw.circle(gameDisplay, white, (400, 300), 8)  # (x, y), radius
+    pygame.draw.rect(gameDisplay, black, (750, y2, 10, 125))
 
     cx = (gameDisplay.get_width() / 2)-50
     cy = (gameDisplay.get_height() - 590)
